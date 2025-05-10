7a2ed344519b9bfdb15f4b479665658da7ebabd5
JosielCM <93870159+josielcm@users.noreply.github.com>
2025-05-10 13:24:52 +0000

    lombok truco

---
+++ bukkit/src/main/java/me/josielcm/jcm/api/formats/Format.java
@@ -0,0 +1,22 @@
+package me.josielcm.jcm.api.formats;
+
+import java.util.regex.Pattern;
+
+public class Format {
+
+    private static final Pattern SPACING_CHARS_REGEX = Pattern.compile("[_ \\-]+");
+    
+    public static String formatTime(int time) {
+        int days = time / 86400;
+        int hours = time / 3600;
+        int minutes = time / 60;
+        int seconds = time % 60;
+        
+        return String.format("%02d:%02d:%02d:%02d", days, hours, minutes, seconds);
+    }
+
+    public static String removeSpacingChars(String string) {
+		return SPACING_CHARS_REGEX.matcher(string).replaceAll("");
+	}
+
+}
